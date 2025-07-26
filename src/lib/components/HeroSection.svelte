e19d0c6ab5de5b91b21fc660663368b1e10cb7a0
JulioRobot <juliorobot16@gmail.com>
2025-07-26 14:19:50 +0700

    video-77

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import heroImage from '$assets/hero.png';
+</script>
+
+<section class="default-margin hero">
+    <div class="hero-text">
+        <h1>Welcome to Book Nest</h1>
+        <h3>Your ultimate book management solution</h3>
+        <h4 class="mt-1">
+            Create your very own digital library where you can keep track of every
+            book you own, read and love.
+        </h4>
+        <h4 class="mb-s">
+            Our app offers a beautifully designed, easy-to-use interface that makes
+            managing your book collection a joy
+        </h4>
+        <button>Sign up</button>
+    </div>
+    <img class="hero-image" src={heroImage} alt="Hero Image" />
+</section>
---
+++ src/lib/components/index.ts
@@ -0,0 +1,2 @@
+// place files you want to import through the `$components` alias in this folder.
+export { default as HeroSection } from './HeroSection.svelte';
