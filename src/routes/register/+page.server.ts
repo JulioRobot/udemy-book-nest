563bacc0913616b6ef692ce5beb56e4402d6ca0e
JulioRobot <juliorobot16@gmail.com>
2025-08-06 00:57:31 +0700

    asnync

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -3,7 +3,7 @@ import type { Actions } from "./$types";
 export const actions = {
     default: async ({ request }) => {
         const formData = await request.formData();
-        
+
         const name = formData.get("name") as string;
         const email = formData.get("email") as string;
         const password = formData.get("password") as string;
